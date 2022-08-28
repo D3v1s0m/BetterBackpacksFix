479b2c062796831bf9956f4eff1ff876c9045d4d
Om Choksi <omchoksi123@gmail.com>
2022-08-29 00:04:01 +0530

    removed redundant defaultAliases variable.

--- src/main/java/me/d3v1s0m/betterbackpacksfix/BetterBackpacksFix.java
+++ src/main/java/me/d3v1s0m/betterbackpacksfix/BetterBackpacksFix.java
@@ -9,7 +9,6 @@ import me.d3v1s0m.betterbackpacksfix.listeners.InventoryList;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,22 +17,16 @@ public final class BetterBackpacksFix extends JavaPlugin {
 
     public static BetterBackpacksFix plugin;
 
-    public static List<String> defaultAliases = new ArrayList<>();
-
     @Override
     public void onEnable() {
         // Plugin startup logic
-        plugin = this;
+        plugin = (BetterBackpacksFix) this;
         if (getServer().getPluginManager().getPlugin("BetterBackpacks") == null) {
             getLogger().severe("BetterBackpacks not found! Disabling plugin.");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
         Bukkit.getConsoleSender().sendMessage("Enabling BetterBackpacksFix ...");
-        defaultAliases.add("betterbackpackfix");
-        defaultAliases.add("bbackpacksfix");
-        defaultAliases.add("bbackpackfix");
-        defaultAliases.add("bbpf");
         Configuration.setup();
         setDefaults();
         Configuration.save();
